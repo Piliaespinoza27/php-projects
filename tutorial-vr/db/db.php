e0d668ff4a449340c6cad7f53535fb7ecac4a8b6
Piliaespinoza27 <61468865+Piliaespinoza27@users.noreply.github.com>
2020-12-12 14:51:42 -0600

    Update db.php

--- tutorial-vr/db/db.php
+++ tutorial-vr/db/db.php
@@ -1,7 +1,7 @@
 <?php
 class Conectar{
     public static function conexion(){
-        $conexion=new mysqli("localhost", "root", "angelitho22", "mvc");
+        $conexion=new mysqli("localhost", "root", "angelitho28", "mvc");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
